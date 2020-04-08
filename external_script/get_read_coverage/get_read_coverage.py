4dfef402dec853e981bb69b55d75af59155ea6fc
leandro <leandro@ebi.ac.uk>
2020-04-08 14:59:07 +0100

    Adding script to get the read coverage

---
+++ external_script/get_read_coverage/get_read_coverage.py
@@ -0,0 +1,31 @@
+import argparse
+import pysam
+import pandas as pd
+
+
+def get_number_of_bases(file):
+    with pysam.FastxFile(file) as fastx_file:
+        number_of_bases = sum([len(entry.sequence) for entry in fastx_file])
+    return number_of_bases
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Get read coverage given assembly and reads.')
+    parser.add_argument('--assembly_and_reads_csv', type=str, required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    assembly_and_reads_csv = pd.read_csv(args.assembly_and_reads_csv)
+
+    for assembly, reads in zip(assembly_and_reads_csv["assembly"], assembly_and_reads_csv["reads"]):
+        number_of_bases_in_assembly = get_number_of_bases(assembly)
+        number_of_bases_in_read = get_number_of_bases(reads)
+        coverage = number_of_bases_in_read / number_of_bases_in_assembly
+        print(f"Coverage of {reads} on {assembly}:")
+        print(coverage)
+
+if __name__ == "__main__":
+    main()
