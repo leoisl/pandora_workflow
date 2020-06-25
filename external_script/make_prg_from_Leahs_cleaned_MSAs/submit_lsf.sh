4b68dd6b5cfdd244b3418e767d9c7c768661cdb4
leandro <leandro@ebi.ac.uk>
2020-06-25 16:39:17 +0100

    Updating submission script

--- external_script/make_prg_from_Leahs_cleaned_MSAs/submit_lsf.sh
+++ external_script/make_prg_from_Leahs_cleaned_MSAs/submit_lsf.sh
@@ -3,13 +3,15 @@ set -eux
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 LOG_DIR=logs/
 MEMORY=8000
+LOCAL_CORES=10
 PROFILE="lsf"
 
 mkdir -p "$LOG_DIR"
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+    -n "$LOCAL_CORES" \
     -M "$MEMORY" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
-      snakemake --profile "$PROFILE" --stats "$LOG_DIR"/stats "$@"
+      snakemake --local-cores "$LOCAL_CORES" --profile "$PROFILE" --stats "$LOG_DIR"/stats "$@"
 
