c579bd9a0d8783b6db6f13979dd498e31b3fcb85
leandro <leandro@ebi.ac.uk>
2020-07-02 05:57:39 +0100

    Fixing pipeline to use only one Snakefile

--- scripts/submit_lsf.sh
+++ scripts/submit_lsf.sh
@@ -3,6 +3,7 @@ set -eux
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 LOG_DIR=logs/
 MEMORY=16000
+PROFILE="lsf"
 
 mkdir -p "$LOG_DIR"
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
@@ -10,5 +11,5 @@ bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
-      bash scripts/run_pipeline.sh "$@"
+    snakemake --profile "$PROFILE" --stats "$LOG_DIR"/Snakefile_compare.stats "$@"
 
