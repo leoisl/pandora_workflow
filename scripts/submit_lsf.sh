463d9c43bc996500892f4b53312470aca610c87a
leandro <leandro@ebi.ac.uk>
2020-07-03 20:13:10 +0100

    Reverting Fixing pipeline to use only one Snakefile

--- scripts/submit_lsf.sh
+++ scripts/submit_lsf.sh
@@ -3,7 +3,6 @@ set -eux
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 LOG_DIR=logs/
 MEMORY=16000
-PROFILE="lsf"
 
 mkdir -p "$LOG_DIR"
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
@@ -11,5 +10,5 @@ bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
-    snakemake --profile "$PROFILE" --stats "$LOG_DIR"/Snakefile_compare.stats "$@"
+      bash scripts/run_pipeline.sh "$@"
 
