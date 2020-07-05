6a8aa5b022705a1cc4e8089caaf99fbfd84283a1
leandro <leandro@ebi.ac.uk>
2020-07-05 15:13:56 +0100

    Adding script to run this pipeline locally

---
+++ scripts/run_pipeline_local.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eux
+
+CORES=112
+LOG_DIR=logs/
+
+
+mkdir -p "$LOG_DIR"
+snakemake -j "$CORES" --snakefile Snakefile_map_with_denovo --stats "$LOG_DIR"/Snakefile_map_with_denovo.stats --use-singularity >"$LOG_DIR"/Snakefile_map_with_denovo.out 2>"$LOG_DIR"/Snakefile_map_with_denovo.err
+sleep 60 # avoid locked directory issues
+snakemake -j "$CORES" --snakefile Snakefile_get_denovo_updated_prg --stats "$LOG_DIR"/Snakefile_get_denovo_updated_prg.stats --use-singularity >"$LOG_DIR"/Snakefile_get_denovo_updated_prg.out 2>"$LOG_DIR"/Snakefile_get_denovo_updated_prg.err
+sleep 60 # avoid locked directory issues
+snakemake -j "$CORES" --snakefile Snakefile_compare --stats "$LOG_DIR"/Snakefile_compare.stats --use-singularity >"$LOG_DIR"/Snakefile_compare.out 2>"$LOG_DIR"/Snakefile_compare.err
