6844c991de77144834018cbb7297a9a3fd748c56
leandro <leandro@ebi.ac.uk>
2020-07-07 16:17:07 +0100

    Fixing bug in cp_or_run_MSA.py

--- external_script/make_prg_from_Leahs_cleaned_MSAs/cp_or_run_MSA.py
+++ external_script/make_prg_from_Leahs_cleaned_MSAs/cp_or_run_MSA.py
@@ -1,6 +1,15 @@
 from snakemake import shell
 from pathlib import Path
 
+def there_is_only_one_sequence(fasta_file):
+    nb_of_seqs = 0
+    with open(fasta_file) as fasta_file_fh:
+        for line in fasta_file_fh:
+            if line.startswith(">"):
+                nb_of_seqs += 1
+    return nb_of_seqs == 1
+
+
 def main():
     previous_gene = Path(snakemake.input.previous_gene)
     msa = Path(snakemake.output.msa)
@@ -10,6 +19,9 @@ def main():
     if not run_MSA:
         shell(f"cp {previous_gene} {msa}")
     else:
-        shell(f"clustalo --dealign --threads {threads} --in {previous_gene} --out {msa}")
+        if there_is_only_one_sequence(previous_gene):
+            shell(f"cp {previous_gene} {msa}")
+        else:
+            shell(f"clustalo --dealign --threads {threads} --in {previous_gene} --out {msa}")
 
 main()
