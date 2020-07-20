e9b898d027ef12d1a5186ba10a62149db534dd02
leandro <leandro@ebi.ac.uk>
2020-07-20 12:53:17 +0100

    Snakemake now creating report files

--- scripts/run_pipeline_local.sh
+++ scripts/run_pipeline_local.sh
@@ -6,8 +6,8 @@ LOG_DIR=logs/
 
 
 mkdir -p "$LOG_DIR"
-snakemake -j "$CORES" --snakefile Snakefile_map_with_denovo --stats "$LOG_DIR"/Snakefile_map_with_denovo.stats --use-singularity >"$LOG_DIR"/Snakefile_map_with_denovo.out 2>"$LOG_DIR"/Snakefile_map_with_denovo.err
+snakemake -j "$CORES" --snakefile Snakefile_map_with_denovo --stats "$LOG_DIR"/Snakefile_map_with_denovo.stats --report "$LOG_DIR"/Snakefile_map_with_denovo.report.html  --use-singularity >"$LOG_DIR"/Snakefile_map_with_denovo.out 2>"$LOG_DIR"/Snakefile_map_with_denovo.err
 sleep 60 # avoid locked directory issues
-snakemake -j "$CORES" --snakefile Snakefile_get_denovo_updated_prg --stats "$LOG_DIR"/Snakefile_get_denovo_updated_prg.stats --use-singularity >"$LOG_DIR"/Snakefile_get_denovo_updated_prg.out 2>"$LOG_DIR"/Snakefile_get_denovo_updated_prg.err
+snakemake -j "$CORES" --snakefile Snakefile_get_denovo_updated_prg --stats "$LOG_DIR"/Snakefile_get_denovo_updated_prg.stats --report "$LOG_DIR"/Snakefile_get_denovo_updated_prg.report.html --use-singularity >"$LOG_DIR"/Snakefile_get_denovo_updated_prg.out 2>"$LOG_DIR"/Snakefile_get_denovo_updated_prg.err
 sleep 60 # avoid locked directory issues
-snakemake -j "$CORES" --snakefile Snakefile_compare --stats "$LOG_DIR"/Snakefile_compare.stats --use-singularity >"$LOG_DIR"/Snakefile_compare.out 2>"$LOG_DIR"/Snakefile_compare.err
+snakemake -j "$CORES" --snakefile Snakefile_compare --stats "$LOG_DIR"/Snakefile_compare.stats --report "$LOG_DIR"/Snakefile_compare.report.html --use-singularity >"$LOG_DIR"/Snakefile_compare.out 2>"$LOG_DIR"/Snakefile_compare.err
