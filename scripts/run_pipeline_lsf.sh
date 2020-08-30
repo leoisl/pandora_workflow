5f0a9687f21f826d64b8a8c02607e539c61f2b9c
leandro <leandro@ebi.ac.uk>
2020-08-30 15:54:55 +0100

    need several cores to speed up

--- scripts/run_pipeline_lsf.sh
+++ scripts/run_pipeline_lsf.sh
@@ -3,10 +3,11 @@ set -eux
 
 PROFILE="lsf"
 LOG_DIR=logs/
+LOCAL_CORES=16
 
-snakemake --snakefile Snakefile_map_with_denovo        --profile "$PROFILE" --stats "$LOG_DIR"/Snakefile_map_with_denovo.stats "$@"
+snakemake --snakefile Snakefile_map_with_denovo        --profile "$PROFILE" --local-cores "$LOCAL_CORES" --stats "$LOG_DIR"/Snakefile_map_with_denovo.stats "$@"
 sleep 60 # avoid locked directory issues
-snakemake --snakefile Snakefile_get_denovo_updated_prg --profile "$PROFILE" --stats "$LOG_DIR"/Snakefile_get_denovo_updated_prg.stats --keep-going "$@"
+snakemake --snakefile Snakefile_get_denovo_updated_prg --profile "$PROFILE" --local-cores "$LOCAL_CORES" --stats "$LOG_DIR"/Snakefile_get_denovo_updated_prg.stats --keep-going "$@"
 sleep 60 # avoid locked directory issues
-snakemake --snakefile Snakefile_compare                --profile "$PROFILE" --stats "$LOG_DIR"/Snakefile_compare.stats "$@"
+snakemake --snakefile Snakefile_compare                --profile "$PROFILE" --local-cores "$LOCAL_CORES" --stats "$LOG_DIR"/Snakefile_compare.stats "$@"
 exit 0
--- scripts/submit_lsf.sh
+++ scripts/submit_lsf.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-MEMORY=8000
+MEMORY=16000
+LOCAL_CORES=16
 LOG_DIR=logs/
 JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
 
@@ -9,6 +10,7 @@ mkdir -p "$LOG_DIR"
 
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
     -M "$MEMORY" \
+    -n "$LOCAL_CORES" \
     -o "$LOG_DIR"/"$JOB_NAME".o \
     -e "$LOG_DIR"/"$JOB_NAME".e \
     -J "$JOB_NAME" \
