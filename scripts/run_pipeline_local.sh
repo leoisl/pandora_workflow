87480be681815976eaceba4f146fff1495ba5c82
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-03-28 15:54:07 +0100

    Updating pipeline to use pandora v0.9.0 and make_prg v0.2.0_prototype

--- scripts/run_pipeline_local.sh
+++ scripts/run_pipeline_local.sh
@@ -3,10 +3,5 @@ set -eux
 
 LOG_DIR=logs/
 mkdir -p "$LOG_DIR"
-
-snakemake --snakefile Snakefile_map_with_denovo        --use-singularity "$@"
-sleep 60 # avoid locked directory issues
-snakemake --snakefile Snakefile_get_denovo_updated_prg --use-singularity "$@"
-sleep 60 # avoid locked directory issues
-snakemake --snakefile Snakefile_compare                --use-singularity "$@"
+snakemake --use-singularity "$@"
 exit 0
